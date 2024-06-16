5f0a7ff62b27ac6ed63bd3faedbe7a63e3432c47
PacHOOk <pachook@interia.pl>
2024-06-16 21:31:12 +0200

    Project structure initialized

---
+++ src/main/java/pl/beusable/room_occupancy_manager/RoomOccupancyManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.beusable.room_occupancy_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupancyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/beusable/room_occupancy_manager/RoomOccupancyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beusable.room_occupancy_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomOccupancyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
